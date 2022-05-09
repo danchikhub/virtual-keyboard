86d3e32d3b87701728cc20887340cae3b9f12e21
unknown <danchikhub>
2022-05-09 13:49:30 +0600

    feat: add code

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    static: './dist',
+  },
+  plugins: 
+    [new HtmlWebpackPlugin({
+      title: 'Virtual Keyboard',
+      favicon: './src/assets/favicon.ico',
+      hash: true,
+      minify: true
+    })],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ]
+      }
+    ]
+  },
+  optimization: {
+    minimize: true,
+  }
+};
